2763274ffc2e97a5c8ad3c80be56fc1d8d001343
fluidbyte <kent@fluidbyte.net>
2019-04-05 13:56:11 -0500

    fix(script): correct flags

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ docker build . -t dev --build-arg username=$USERNAME
 # RUN
 docker run --privileged -it --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $PWD/dev-home:/home/$USERNAME --hostname=dev -e 3000:3000 -e 8000-9000:8000-9000 dev \
+  -v $PWD/dev-home:/home/$USERNAME --hostname=dev -p 3000:3000 -p 8000-8100:8000-8100 dev \
   /bin/zsh -c "sleep 3 && code-server /home/$USERNAME/workspace -p 3000 -d /home/$USERNAME/code-server --password=$PASSWORD"
