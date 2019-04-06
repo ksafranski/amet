65dc2eb54689dd0d41910aec65c3c2e415264d8b
TomFrost <tom@frosteddesign.com>
2019-04-06 15:37:59 -0400

    fix(entrypoint): Don't ignore docker commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+set -e
+
 sudo service docker start
 code-server /home/$DEV_USERNAME/workspace \
             -p 3000 \
             -d /home/$DEV_USERNAME/code-server \
-            --password=$DEV_PASSWORD
+            --password=$DEV_PASSWORD > /var/log/code-server/exec.log 2>&1 &
+
+exec "$@"
+
