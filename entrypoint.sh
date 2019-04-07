c1fb7a8ef1744ac01c5e0bf5e173b692c9d8a2e2
TomFrost <tom@frosteddesign.com>
2019-04-07 02:46:09 -0400

    feat(entrypoint): Start ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,7 @@
 set -e
 
 sudo service docker start
-code-server /home/$DEV_USERNAME/workspace \
-            -p 3000 \
-            -d /home/$DEV_USERNAME/code-server \
-            --password=$DEV_PASSWORD > /var/log/code-server/exec.log 2>&1 &
+sudo service ssh start
 
 exec "$@"
 
