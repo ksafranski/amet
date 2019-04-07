1af45374953300aa2bde14d5362ee4015a15922d
TomFrost <tom@frosteddesign.com>
2019-04-07 02:46:33 -0400

    feat(run.sh): All the things.

--- README.md
+++ README.md
@@ -10,18 +10,22 @@ development environment.
 
 ## Quick Start
 
-Make the script (`run.sh`) executable, then run the script, supplying desired `username` and `password` 
-for your development environment.
+Run the script, supplying desired `password` for your development environment.
 
-```bash
-chmod +x run.sh
-./run.sh -u <username> -p <password> [-s <shell>]
+```shell
+./run.sh -p <password> [-s <shell>]
 ```
 
 _Note: the `<shell>` can be `bash` or `zsh` (default: `bash`)_
 
 After this script completes the editor will be running at `https://<HOST|IP>:3000`. It will prompt 
-you for the password you entered when running the script.
+you for the password you entered when running the script. Prefer the command line? Hop right in with:
+
+```shell
+ssh -p 3022 localhost
+```
+
+Just enter the password you specified above.
 
 ## Customizing the Environment
 
@@ -30,12 +34,11 @@ the best way to customize your environment is to customize the [`./Dockerfile`](
 
 ## Docker-in-Docker
 
-The Docker container builds the docker client, then, when started, the script runs the docker command passing the 
-`/var/run/docker.sock` as a volume, allowing utilization of the host's docker instantiation within the container.
+The Docker container builds a docker client that can be used without conflicting with the host docker instance.
 
 ## Persisting Data
 
-When the environment starts up it creates a `./dev-env` directory which is mounted as a volume in the 
+If you pass the `-l` flag to `run.sh`, it creates a `./dev-env` directory which is mounted as a volume in the 
 container. This directory is mounted to the `$HOME` path inside of the container.
 
 The root of this directory is your working home directory and can be treated as such; containing other 
@@ -46,3 +49,12 @@ directories, rc's, dot-files, etc. The core directories setup are:
 
 Since the home directory is mounted as a volume all work, changes, configurations, etc will be maintained 
 in the `./dev-env` directory on the host machine and persist when the container is not running.
+
+Want to persist just certain files and folders, relative to your user's home folder? Pass arguments like
+`-m .ssh -m Projects` to the `run.sh` and they'll appear in the same location in the container.
+
+When you're ready to stop developing, you can shut down and restart the container without losing data, regardless
+of whether or not you're persisting volumes. Simply `docker stop USERNAME-dev` (fill in your username!) when you're
+done, and `docker start USERNAME-dev` when you're ready to start again. All your changes from the last run will
+be waiting for you.
+
--- run.sh
+++ run.sh
@@ -1,77 +1,82 @@
 #!/bin/bash
+set -e
 
-USERNAME=user
-PASSWORD=password
-SHELL=bash
-RUN_ARGS="-d"
-MOUNT_LOCAL=0
-FORCE_REBUILD=0
+username=$(whoami)
+password=password
+shell=bash
+runArgs="-d"
+portRangeArgs=""
+volumeArgs=""
+homeVolumes=()
+forceRebuild=0
+appPort=3000
+sshPort=3022
+mountHome=0
 
 # HELP, I NEED SOMEBODY
 showHelp() {
-  echo "Usage: $(basename $0) [-u username] [-p password] [-s shell] [-i] [-m] [-f]"
+  echo "Usage: $(basename $0) ARGUMENTS"
   echo ""
-  echo "  Use -u, -p, and -s to set up your environment. In addition:"
+  echo "  Available arguments:"
   echo ""
-  echo "  -i  Launch the container interactively and open the shell prompt."
-  echo "      Otherwise, the container will run in daemon mode."
-  echo "  -m  Mount the .ssh and .aws directories from the host as volumes in"
-  echo "      the container. Note that modifying the contents of these inside"
-  echo "      the container will persist back to the host."
-  echo "  -f  Force a rebuild of the docker image, even if the tag already exists."
+  echo "  -a <port>   The port that code-server should be exposed on. Defaults to $appPort."
+  echo "  -f          Force a rebuild of the docker image, even if the tag already exists."
+  echo "  -h          Display this help, exit 0"
+  echo "  -i          Launch the container interactively and show logs. Otherwise, the container"
+  echo "              will run in daemon mode."
+  echo "  -l          Locally mount ./dev-home to the user's home folder in the container"
+  echo "  -m <dir>    Mount the given directory (relative to the host user's \$HOME) to the container"
+  echo "              user's \$HOME as a docker volume. Useful to bring in dotfiles and folders like"
+  echo "              .ssh or .vimrc. Can be specified multiple times." 
+  echo "  -o <ranges> Opens the specified port ranges to the host machine. Ex: -o 8000-8100."
+  echo "              Can be specified multiple times."
+  echo "  -p <passwd> The password to set for the user and the code-server instance."
+  echo "              Defaults to '$password'."
+  echo "  -r <port>   The port that remote ssh connections can be established on." \
+  echo "              Defaults to $sshPort." \
+  echo "  -s <shell>  The shell to set for the user. Defaults to '$shell'."
+  echo "  -u <user>   The user to create in the container. Defaults to '$username'."
   echo ""
 }
 
 # PARSE COMMAND LINE ARGS
-while getopts ':fhimp:s:u:' OPT; do
+while getopts ':a:film:o:p:s:u:' OPT; do
   case "$OPT" in
-    h)
-      showHelp
-      exit 0
-      ;;
-    u)
-      USERNAME="$OPTARG"
-      ;;
-    p)
-      PASSWORD="$OPTARG"
-      ;;
-    s)
-      SHELL="$OPTARG"
-      ;;
-    i)
-      RUN_ARGS="-it"
-      ;;
-    m)
-      MOUNT_LOCAL=1
-      ;;
-    f)
-      FORCE_REBUILD=1
-      ;;
-    ?)
-      showHelp
-      exit 1
-      ;;
+    a) appPort="$OPTARG" ;;
+    f) forceRebuild=1 ;;
+    h) showHelp; exit 0 ;;
+    i) runArgs="-it" ;;
+    l) mountHome=1 ;;
+    m) homeVolumes+=($OPTARG) ;;
+    o) portRangeArgs+="-p $OPTARG:$OPTARG " ;;
+    p) password="$OPTARG" ;;
+    s) shell="$OPTARG" ;;
+    u) username="$OPTARG" ;;
+    ?) showHelp; exit 1 ;;
   esac
 done
-shift "$(($OPTIND -1))"
 
-# MOUNT LOCAL CONFIG FOLDERS
-if [ $MOUNT_LOCAL -eq 1 ]; then
-  [ -d "$HOME/.ssh" ] && RUN_ARGS="$RUN_ARGS -v $HOME/.ssh:/home/$USERNAME/.ssh"
-  [ -d "$HOME/.aws" ] && RUN_ARGS="$RUN_ARGS -v $HOME/.aws:/home/$USERNAME/.aws"
-fi
+# PROCESS HOME VOLUMES INTO VOLUME ARGS
+[ $mountHome -eq 1 ] && volumeArgs="-v $PWD/dev-home:/home/$username"
+for dir in "${homeVolumes[@]}"; do
+  volumeArgs+="-v $HOME/$dir:/home/$username/$dir "
+done
 
 # BUILD
-IMG_QUERY=$(docker images -q dev 2>/dev/null)
-if [[ $IMG_QUERY == "" ]] || [ $FORCE_REBUILD -eq 1 ]; then
-  docker build . -t dev --build-arg username=$USERNAME --build-arg password=$PASSWORD --build-arg shell=$SHELL
+imageQuery=$(docker images -q dev 2>/dev/null)
+if [[ $imageQuery == "" ]] || [ $forceRebuild -eq 1 ]; then
+  docker build . -t dev \
+    --build-arg username=$username \
+    --build-arg password=$password \
+    --build-arg shell=$shell
 fi
 
 # RUN
-docker run --privileged $RUN_ARGS \
-  -v $PWD/dev-home:/home/$USERNAME \
-  --hostname=$USERNAME-dev \
-  --name=$USERNAME-dev \
-  -p 8000-8100:8000-8100 \
-  -p 3000:3000 \
-  dev /bin/$SHELL
+docker run --privileged $runArgs \
+  $volumeArgs \
+  --hostname=${username}-dev \
+  --name=${username}-dev \
+  -p ${appPort}:3000 \
+  -p ${sshPort}:22 \
+  $portRangeArgs \
+  dev
